617f8894ea42f8cde523bfeeb09989c0d7c4cc28
jaipur <yogendra.jain@allonblock.com>
2019-01-26 12:09:04 +0530

    first commit

---
+++ precache-manifest.a1897a7589da872275657f13a6e67c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "749b4d069bbfb73205ee525604546cc6",
+    "url": "/static/media/shubhibirthday.749b4d06.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a7bd046d2ff8e1c0a99",
+    "url": "/static/js/main.9a7bd046.chunk.js"
+  },
+  {
+    "revision": "125a0f569b8ec066cb2e",
+    "url": "/static/js/1.125a0f56.chunk.js"
+  },
+  {
+    "revision": "9a7bd046d2ff8e1c0a99",
+    "url": "/static/css/main.dfd6894f.chunk.css"
+  },
+  {
+    "revision": "da0d56f53d2d8e827acea43180db6b6f",
+    "url": "/index.html"
+  }
+];
